1b547e522e395185b1c1f6b7a01c12b938a5c233
freddyrosa16 <freddy.rosa16@gmail.com>
2024-05-03 23:08:50 -0400

    Created a script to represent a fortune cookie

---
+++ Fortune Cookie/fortune_cookie.py
@@ -0,0 +1,7 @@
+import random
+
+fortune = ['Tomorrow will be a better day', 'The money comes and goes, but you are forever',
+           'The good thing about hitting rock bottom is that you can only go up', 'Watch out how you talk too yourself']
+
+cookie = random.choice(fortune)
+print(cookie)
